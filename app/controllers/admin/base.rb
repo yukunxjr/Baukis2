a40361809cece445f767ce8a9a43901345da274e
ダトラ共通 <andouyuuto@andouyuutonoMacBook-Pro.local>
2023-04-08 12:05:45 +0900

    Chapter12_practice 完了

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,5 +1,7 @@
 class Admin::Base < ApplicationController
   before_action :authorize
+  before_action :check_account
+  before_action :check_timeout
 
   private def current_adimnstrator
     if session[:adimnstrator_id]
@@ -12,6 +14,28 @@ class Admin::Base < ApplicationController
     unless current_adimnstrator
         flash.alert = "管理者としてログインしてください。"
         redirect_to :admin_login
+    end
+  end
+
+  private def check_account
+    if current_adimnstrator && current_adimnstrator.suspended?
+      session.delete(:adimnstrator_id)
+      flash.alert = "アカウントが無効になりました。"
+      redirect_to :admin_root
+    end
+  end
+
+  TIMEOUT = 60.minute
+
+  private def check_timeout
+    if current_adimnstrator
+      if session[:admin_last_access_time] >= TIMEOUT.ago
+        session[:admin_last_access_time] = Time.current
+      else
+        session.delete(:adimnstrator_id)
+        flash.alert = "セッションがタイムアウトしました。"
+        redirect_to :admin_login
+      end
     end
   end
 
